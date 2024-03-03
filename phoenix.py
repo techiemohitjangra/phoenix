4dbe76c6c7cf60194ef6755d764d7ae64b056c69
Mohit Jangra <mohitjangra12@gmail.com>
2024-03-03 16:48:49 +0530

    Update: improved package install

--- phoenix.py
+++ phoenix.py
@@ -2,23 +2,26 @@
 
 import os
 
+common_packages = ["ripgrep", "kitty", "valgrind"]
+
 # =============================== Arch Setup ==================================
-arch_packages = ["kitty", "rust", "cargo", "golang", "nodejs", "npm", "neovim",
+arch_packages = ["rust", "cargo", "go", "nodejs", "npm", "neovim",
                  "tmux", "git", "make", "clang", "gcc", "firefox", "fzf",
                  "xclip", "ttf-jetbrains-mono-nerd", "ttf-ubuntu-mono-nerd",
                  "ttf-ubuntu-nerd", "ttf-roboto-mono-nerd",
                  "ttf-nerd-fonts-symbols", "ttf-nerd-fonts-symbols-mono",
                  "ttf-tinos-nerd", "zsh", "curl", "wget", "man-db", "unzip",
-                 "ffmpeg", "feh", "screenkey"]
-os.system("pacman --version &>/dev/null")
-exit_code = os.system("$?")
-if exit_code == 0:
+                 "ffmpeg", "feh", "screenkey", "chezmoi"]
+exit_code = os.system("pacman --version &>/dev/null")
+if int(exit_code) == 0:
     PACKAGE_MANAGER = "pacman"
+    for package in common_packages:
+        os.system(f"pacman -S --needed {package} -y")
     for package in arch_packages:
-        os.system(f"pacman -Sy {package}")
+        os.system(f"pacman -S --needed {package} -y")
 
 # ============================== Debian Setup =================================
-debian_packages = ["kitty", "rustc", "cargo", "golang", "nodejs", "npm",
+debian_packages = ["rustc", "cargo", "golang", "nodejs", "npm",
                    "neovim", "tmux", "git", "make", "clang", "gcc", "firefox",
                    "fzf", "xclip", "ttf-jetbrains-mono-nerd",
                    "ttf-ubuntu-mono-nerd", "ttf-ubuntu-nerd",
@@ -27,16 +30,20 @@ debian_packages = ["kitty", "rustc", "cargo", "golang", "nodejs", "npm",
                    "curl", "wget", "man-db", "unzip", "ffmpeg", "feh",
                    "screenkey"]
 
-os.system("apt --version &>/dev/null")
-exit_code = os.system("$?")
+exit_code = os.system("apt --version &>/dev/null")
 if exit_code == 0:
     PACKAGE_MANAGER = "apt"
+    for package in common_packages:
+        os.system(f"apt install {package} -y")
     for package in debian_packages:
-        os.system(f"apt install {package}")
+        os.system(f"apt install {package} -y")
+
+    # install chezmoi
+    os.system("sh -c \"$(curl -fsLS get.chezmoi.io)\"")
 
 
 # ============================== Fedora Setup =================================
-fedora_packages = ["kitty", "rustc", "cargo", "golang", "nodejs", "npm",
+fedora_packages = ["rustc", "cargo", "golang", "nodejs", "npm",
                    "neovim", "tmux", "git", "make", "clang", "gcc", "firefox",
                    "fzf", "xclip", "ttf-jetbrains-mono-nerd",
                    "ttf-ubuntu-mono-nerd", "ttf-ubuntu-nerd",
@@ -44,9 +51,11 @@ fedora_packages = ["kitty", "rustc", "cargo", "golang", "nodejs", "npm",
                    "ttf-nerd-fonts-symbols-mono", "ttf-tinos-nerd", "zsh",
                    "curl", "wget", "man-db", "unzip", "ffmpeg", "feh",
                    "screenkey"]
-os.system("dnf --version &>/dev/null")
-exit_code = os.system("$?")
+exit_code = os.system("dnf --version &>/dev/null")
 if exit_code == 0:
     PACKAGE_MANAGER = "dnf"
     for package in fedora_packages:
         os.system(f"dnf install {package}")
+
+    # install chezmoi
+    os.system("sh -c \"$(curl -fsLS get.chezmoi.io)\"")
